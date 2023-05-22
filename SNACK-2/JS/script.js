2d234e2b8d0b1d8e7ddeb49c720132fa7fa0355c
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-22 10:34:37 +0200

    change numberofattemps = 1

--- SNACK-2/JS/script.js
+++ SNACK-2/JS/script.js
@@ -1,24 +1,26 @@
 
-let numberofattempts = 0
+
 computernumber = Math.round(Math.random() * 100 +1);
 console.log(computernumber);
-usernumber = parseInt(prompt("Write a number"))
+usernumber = parseInt(prompt("Write a number"));
+let numberofattempts = 1;
 
 
 while (usernumber != computernumber){
     
     if (usernumber < computernumber){
   
-        alert("Your number is smaller than the number of the computer,please try again")
-        usernumber = parseInt(prompt("Write a number"))
+        alert("Your number is smaller than the number of the computer,please try again");
+        usernumber = parseInt(prompt("Write a number"));
         console.log(usernumber);
     } else if (usernumber > computernumber){
-        alert("Your number is bigger than the number of the computer, please try again")
-        usernumber = parseInt(prompt("Write a number"))
+        alert("Your number is bigger than the number of the computer, please try again");
+        usernumber = parseInt(prompt("Write a number"));
         console.log(usernumber);
     }
 
     usernumber === computernumber;
+    alert("Great! You guessed the number.");
     numberofattempts = numberofattempts + 1;
     console.log(numberofattempts);
 }
