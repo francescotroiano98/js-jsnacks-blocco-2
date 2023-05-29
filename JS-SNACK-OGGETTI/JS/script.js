1539cdda222329234bef15fd30f2a93aa6e67d2a
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-29 11:16:20 +0200

    add basketPlayer

---
+++ JS-SNACK-OGGETTI/JS/script.js
@@ -0,0 +1,13 @@
+
+const basketPlayer = [
+    {
+       codicegiocatore : 1 ,
+       nome : 'Franco',
+       cognome: 'Rossi',
+       'età' : 25,
+       'Tiri': 30,
+       'Media punti fatti per partita' : 25,
+       'Percentuale di successo per tiri da 3 punti':' 30%',
+       'Stoppate' : 10,
+    }
+];
