5d3b74a19600d4a4c9324908762b135e71fc19b7
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-30 12:46:50 +0200

    added for.Each soluction

--- JS-SNACK-OGGETTI/JS/script.js
+++ JS-SNACK-OGGETTI/JS/script.js
@@ -1,13 +1,38 @@
-
-const basketPlayer = [
-    {
-       codicegiocatore : 1 ,
-       nome : 'Franco',
-       cognome: 'Rossi',
-       'età' : 25,
-       'Tiri': 30,
-       'Media punti fatti per partita' : 25,
-       'Percentuale di successo per tiri da 3 punti':' 30%',
-       'Stoppate' : 10,
+const player = {
+    playerId : '',
+    firstName : 'Librona',
+    lastName: 'Giovanna',
+    age: 21,
+    appm : 0,
+    pct3 : 0,
+    blk:0,
+    shots: 0,
+ }
+ 
+ player.appm = getRandomInt(0, 50);
+ player.pct3 = getRandomInt(0, 100);
+ player.blk = getRandomInt(0, 30);
+ player.shots = getRandomInt(20, 100);
+ 
+ player.playerId = generateRandomString(3, 'QWERTYUIOPASDFGHJKLZXCVBNM') + getRandomInt(0,9) + getRandomInt(0,9) + getRandomInt(0,9) ;
+ 
+ console.log(player);
+ 
+ function getRandomInt(minumNumber, maximumNumber){
+    const randomNumber = Math.floor( Math.random() * ( maximumNumber - minumNumber +1) + minumNumber);
+ 
+    return randomNumber;
+ }
+ 
+ function generateRandomString(length, allowedChars ) {
+    let result = '';
+    let counter = 0;
+ 
+    while (counter < length) {
+       result += allowedChars.charAt(Math.floor(Math.random() * allowedChars.length));
+       counter++;
     }
-];
+ 
+    return result;
+ }
+    
